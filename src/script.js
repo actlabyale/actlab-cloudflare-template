1b65dfc4ad926c516a98a6e769de3b0b6589fbdf
Alex Forrence <alex.forrence@gmail.com>
2023-07-05 15:36:59 -0400

    that header would've been useless AFAIK

--- src/script.js
+++ src/script.js
@@ -3,13 +3,13 @@ console.log(import.meta.env.DEV) // true if debug, false if "real"
 document.getElementById('foo').addEventListener('click', async () => {
     console.log('fooo')
     const resp = await fetch('https://api.actlab.dev/template', {
-        method: 'POST',
+        method: 'GET',
         // mode: 'same-origin',
         cache: 'no-cache',
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify({ 'foo': 'bar' })
+        // headers: {
+        //     "content-type": "application/json",
+        // },
+        // body: JSON.stringify({ 'foo': 'bar' })
     })
     console.log(resp)
 })
